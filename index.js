2d32140a223afb69da8d9447c09ff696458a0bee
Cooper <cooperbecon@icloud.com>
2024-12-09 15:19:08 +1100

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🌴 California Love", "🐻 Roarrr"];
+const statusMessages = ["🏰 Long Live Söderrike!", "🎄 Merry Christmas"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
